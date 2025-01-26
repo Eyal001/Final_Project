54e550490eefdf8d2bc43104bd176e24790ccb4b
Eyal001 <eyal.zeitoun@epitech.eu>
2025-01-26 23:25:22 +0200

    Ready for deployment

--- dev-social-platform/backend/src/server.ts
+++ dev-social-platform/backend/src/server.ts
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { db } from "./db/db";
+import path from "path";
 import commentRoute from "./routes/commentRoute";
 import likeCommentRoutes from "./routes/likeCommentRoute";
 import likePostRoute from "./routes/likePostRoute";
@@ -11,35 +11,29 @@ import userRouter from "./routes/userRoute";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT || 5000, () => {
-  console.log(`run on ${PORT || 5001}`);
-});
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
-    credentials: true,
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "http://localhost:3000"],
+//     credentials: true,
+//   })
+// );
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL || "*" }));
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/post-likes", likePostRoute);
 app.use("/api/comments", commentRoute);
 app.use("/api/comment-likes", likeCommentRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
 });
-async function testConnection() {
-  try {
-    const response = await db("users").select("id");
-    console.log("connected to db");
-  } catch (error) {
-    console.log(error);
-  }
-}
-testConnection();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
